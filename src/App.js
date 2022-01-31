b7a0d4abc59739788c399e527c6da4d7b7887b8e
lironshani <75664217+lironshani@users.noreply.github.com>
2022-01-31 18:47:25 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Tower from "../src/components/hanoi/Tower";
+
+function App() {
+  return (
+    <div className="App">
+      <Tower></Tower>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/hanoi/Tower.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import "../hanoi/Tower.css";
+class Tower extends Component {
+  render() {
+    return <div className="tower"></div>;
+  }
+}
+
+export default Tower;
