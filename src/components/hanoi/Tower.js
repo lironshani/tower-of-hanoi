12e087d8bbd54aaa6fde3a3e750efc561cd5dd77
lironshani <75664217+lironshani@users.noreply.github.com>
2022-02-03 17:25:00 +0200

    Add files via upload

--- src/components/hanoi/Tower.js
+++ src/components/hanoi/Tower.js
@@ -2,7 +2,11 @@ import React, { Component } from "react";
 import "../hanoi/Tower.css";
 class Tower extends Component {
   render() {
-    return <div className="tower"></div>;
+    return <div className="tower">
+    
+        <div className='rod1'></div>
+    <div className='rod2'></div>
+    <div className='rod3'></div></div>;
   }
 }
 
