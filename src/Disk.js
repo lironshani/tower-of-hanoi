498eddf21f102991223aa5a41a951ef84c52b0c9
lironshani <75664217+lironshani@users.noreply.github.com>
2022-02-03 17:25:38 +0200

    Add files via upload

---
+++ src/Disk.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "../hanoi/Disk.css";
+class Disk extends Component {
+  render() {
+    return <div className="disk">
+    </div>;
+  }
+}
+
+export default Disk;
