626865a2d7616b66d787ef87479dc4b6e2878339
lironshani <lironshani12@gmail.com>
2025-01-09 18:27:07 +0200

    changing css, add optimal steps number

--- src/components/Disk/Disk.js
+++ src/components/Disk/Disk.js
@@ -5,10 +5,7 @@ const Disk = (props) => {
   let className = "disk " + props.serialClass;
   if (props.selectedDisk === props.serial) className += " selected";
   return (
-    <div
-      className={className}
-      style={{ width: `${props.serial * 80}px` }}
-    ></div>
+    <div className={className} style={{ width: `${props.serial * 60}%` }}></div>
   );
 };
 
