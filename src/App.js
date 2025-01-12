57f5c5a8e6ab9c992638edacf8f55ac28d953d8f
lironshani <lironshani12@gmail.com>
2025-01-13 00:11:54 +0200

    add won game modal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Tower from "../src/components/hanoi/Tower";
-import HomePageView from "./Views/HomePage/Home";
+import Home from "./Views/HomePage/Home";
 
 function App() {
   return (
     <div id="app" className="App">
-      <HomePageView></HomePageView>
+      <Home></Home>
     </div>
   );
 }
